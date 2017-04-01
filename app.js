5da03d2b96965df65fba359cc6dfa2cda51315e8
DejonGill <dejonjgill@gmail.com>
2017-04-01 11:52:42 -0700

    adding tests

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 require('dotenv').config();
 const cluster = require('cluster');
 
-if (cluster.isMaster) {
-  const cpuCount = 3;
+if (process.env.NODE_ENV === 'production') {
+  if (cluster.isMaster) {
+    const cpuCount = 3;
 
-  for (var i = 0; i < cpuCount; i++) {
-    cluster.fork();
+    for (var i = 0; i < cpuCount; i++) {
+      cluster.fork();
+    }
+  } else {
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const path = require('path');
+    const fs = require('fs');
+    const cors = require('cors');
+    const morgan = require('morgan');
+    const compression = require('compression');
+
+    const router = require('./server');
+
+    const app = express();
+    app.use(morgan('dev'));
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(compression());
+
+    router(app);
+
+    const server = app.listen(3000, () => console.log('App listening on port 3000'));
+
+    module.exports = server;
   }
+
 } else {
   const express = require('express');
   const bodyParser = require('body-parser');
@@ -26,5 +51,7 @@ if (cluster.isMaster) {
 
   router(app);
 
-  app.listen(3000, () => console.log('App listening on port 3000'));
+  const server = app.listen(3000, () => console.log('App listening on port 3000'));
+
+  module.exports = server;
 }
