7307c94da140e400a52e6e59956730984ecb1537
DejonGill <dejonjgill@gmail.com>
2017-04-02 19:18:58 -0700

    refactor for better file size

--- test/routes/users/test-get-single.js
+++ test/routes/users/test-get-single.js
@@ -5,10 +5,10 @@ describe('/users', () => {
   let connection;
 
   beforeEach(() => {
-    delete require.cache[require.resolve('../../app')];
-    delete require.cache[require.resolve('../../server/db/db')];
-    server = require('../../app');
-    let { db } = require('../../server/db/db');
+    delete require.cache[require.resolve('../../../app')];
+    delete require.cache[require.resolve('../../../server/db/db')];
+    server = require('../../../app');
+    let { db } = require('../../../server/db/db');
     connection = db.connection;
   });
 
@@ -17,31 +17,6 @@ describe('/users', () => {
     connection.close();
   });
 
-  describe('/api/users/all', () => {
-    it('should return a json array', (done) => {
-      request(server)
-        .get('/api/users/all')
-        .set('Accept', 'application/json')
-        .expect(res => {
-          res.body.data = Array.isArray(res.body.data);
-        })
-        .expect(200, {
-          data: true
-        }, done);
-    });
-
-    it('should return an array of users', function(done) {
-      request(server)
-        .get('/api/users/all')
-        .set('Accept', 'application/json')
-        .expect(res => {
-          res.body.data = res.body.data[0].full_name.toLowerCase();
-        })
-        .expect(200, {
-          data: 'melendez randolph'
-        }, done);
-    });
-  });
 
   describe('/api/users/id/:id', () => {
     it('shold return an object when passed a known id', (done) => {
