7581b9007f547307d3ec2391c226362907302c90
DejonGill <dejonjgill@gmail.com>
2017-04-13 20:44:46 -0700

    init

--- server/db/authenticated-user/authenticated-user.js
+++ server/db/authenticated-user/authenticated-user.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const authenticatedUser = mongoose.Schema({
   username: {
     type: String,
+    unique: true,
+    lowercase: true,
     required: true
   },
   password: {
@@ -12,4 +15,28 @@ const authenticatedUser = mongoose.Schema({
   permissions: Number
 });
 
-module.exports = authenticatedUser;
+authenticatedUser.pre('save', function(next) {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, function(error, hash) {
+      if (error) { return next(error); }
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+authenticatedUser.methods.comparePassword = function(candidatePw, callback) {
+  bcrypt.compare(candidatePw, this.password, function(err, isMatch) {
+    if (err) { return callback(err); }
+
+    callback(null, isMatch);
+  });
+};
+
+const AuthenticatedUserModel = mongoose.model('authenticatedUser', authenticatedUser);
+
+module.exports = authenticatedUserModel;
