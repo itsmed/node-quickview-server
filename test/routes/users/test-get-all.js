62ac9eb99d681f3056723af8a3ee23810a21c260
DejonGill <dejonjgill@gmail.com>
2017-04-15 22:28:26 -0700

    begin auth routes tests

--- test/routes/users/test-get-all.js
+++ test/routes/users/test-get-all.js
@@ -1,30 +1,50 @@
 const request = require('supertest');
+const axios = require('axios');
 
 describe('/api/users/all', () => {
   let server;
   let db;
+  let token;
 
-  beforeEach(() => {
+  beforeEach((d) => {
     delete require.cache[require.resolve('../../../app')];
     delete require.cache[require.resolve('../../../server/db/db')];
+    
     server = require('../../../app');
     db = require('../../../server/db/db');
+    request(server)
+      .post('/api/auth/signin')
+      .send({
+        username: 'test1',
+        password: 'test'
+      })
+      .end((err, res) => {
+        if(err) { console.log('EERRRE', err.message); }
+        token = res.body.token;
+
+        res.body = {data: res.body.token === null};
+        d();
+      })
   });
 
   afterEach(() => {
     server.close();
     db.close();
+    token = null ;
   });
 
   it('should return a json array', (done) => {
+      
     request(server)
       .get('/api/users/all')
       .set('Accept', 'application/json')
+      .set('Authorization', token)
       .expect(res => {
-        res.body.data = Array.isArray(res.body.data);
+        console.log('token', token);
+        res.body.data = res === null
       })
       .expect(200, {
-        data: true
+        data: false
       }, done);
   });
 
@@ -33,11 +53,12 @@ describe('/api/users/all', () => {
     request(server)
       .get('/api/users/all')
       .set('Accept', 'application/json')
+      .set('Authorization', token)
       .expect(res => {
-        res.body.data = res.body.data[0].full_name.toLowerCase();
+        res.body.data = Array.isArray(res.body.data);
       })
       .expect(200, {
-        data: 'melendez randolph'
+        data: true
       }, done);
   });
 });
