a9402b09e1c80d9cd0be881744a619550c47fd28
DejonGill <dejonjgill@gmail.com>
2017-04-16 18:29:35 -0700

    update

--- test/routes/test-unauthorized-user.js
+++ test/routes/test-unauthorized-user.js
@@ -16,102 +16,120 @@ describe('unauthorized users should not hve access to any api routes', () => {
     db.close();
   });
 
+  describe('/users routes', () => {
+    it('/api/users/all', (done) => {
+      request(server)
+        .get('/api/users/all')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
 
-  it('/api/users/all', (done) => {
-    request(server)
-      .get('/api/users/all')
-      .set('Accept', 'application/json')
-      .expect(200, {
-        error: 'Unauthorized'
-      }, done);
+    it('/api/users/id/:id', (done) => {
+      request(server)
+        .get('/api/users/id/7a5704d7-c44a-4795-aa06-a3c7b6218f8b')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+    it('/api/users/search/email/:email', (done) => {
+      request(server)
+        .get('/api/users/search/email/horton.cote@mobildata.com')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+    it('/api/users/search/name/:name', (done) => {
+      request(server)
+        .get('/api/users/search/name/cote')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+    it('/api/users/search/permissions/:permissions', (done) => {
+      request(server)
+        .get('/api/users/search/email/7')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
   });
 
-  it('/api/users/id/:id', (done) => {
-    request(server)
-      .get('/api/users/id/7a5704d7-c44a-4795-aa06-a3c7b6218f8b')
-      .set('Accept', 'application/json')
-      .expect(200, {
-        error: 'Unauthorized'
-      }, done);
+  describe('/employees routes', () => {
+    it('/api/employees/all', (done) => {
+      request(server)
+        .get('/api/employees/all')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+    it('/api/employees/id/:id', (done) => {
+      request(server)
+        .get('/api/employees/id/7a5704d7-c44a-4795-aa06-a3c7b6218f8b')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+    it('/api/employees/search/email/:email', (done) => {
+      request(server)
+        .get('/api/employees/search/email/horton.cote@mobildata.com')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+    it('/api/employees/search/name/:name', (done) => {
+      request(server)
+        .get('/api/employees/search/name/cote')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
   });
 
-  it('/api/users/search/email/:email', (done) => {
-    request(server)
-      .get('/api/users/search/email/Horton.Cote@Mobildata.com')
-      .set('Accept', 'application/json')
-      .expect(200, {
-        error: 'Unauthorized'
-      }, done);
+  describe('/transactions', () => {
+    it('/api/transactions/all', (done) => {
+      request(server)
+        .get('/api/transactions/all')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+    it('/api/transactions/id/:id', (done) => {
+      request(server)
+        .get('/api/transactions/id/7a5704d7-c44a-4795-aa06-a3c7b6218f8b')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+    it('/api/transactions/user/id/:id', (done) => {
+      request(server)
+        .get('/api/transactions/user/id/fnau89fsfks-t23-fc2jefs')
+        .set('Accept', 'application/json')
+        .expect(200, {
+          error: 'Unauthorized'
+        }, done);
+    });
+
+
   });
-
-
-
-
-
-
-  it('/api/transactions/all', (done) => {
-    request(server)
-      .get('/api/users/all')
-      .set('Accept', 'application/json')
-      .expect(200, {
-        error: 'Unauthorized'
-      }, done);
-  });
-  it('/api/employees/all', (done) => {
-    request(server)
-      .get('/api/users/all')
-      .set('Accept', 'application/json')
-      .expect(200, {
-        error: 'Unauthorized'
-      }, done);
-  });
-  // it('/api/users/all', (done) => {
-  //   request(server)
-  //     .get('/api/users/all')
-  //     .set('Accept', 'application/json')
-  //     .expect(200, {
-  //       error: 'Unauthorized'
-  //     }, done);
-  // });
-  // it('/api/users/all', (done) => {
-  //   request(server)
-  //     .get('/api/users/all')
-  //     .set('Accept', 'application/json')
-  //     .expect(200, {
-  //       error: 'Unauthorized'
-  //     }, done);
-  // });
-  // it('/api/users/all', (done) => {
-  //   request(server)
-  //     .get('/api/users/all')
-  //     .set('Accept', 'application/json')
-  //     .expect(200, {
-  //       error: 'Unauthorized'
-  //     }, done);
-  // });
-  // it('/api/users/all', (done) => {
-  //   request(server)
-  //     .get('/api/users/all')
-  //     .set('Accept', 'application/json')
-  //     .expect(200, {
-  //       error: 'Unauthorized'
-  //     }, done);
-  // });
-  // it('/api/users/all', (done) => {
-  //   request(server)
-  //     .get('/api/users/all')
-  //     .set('Accept', 'application/json')
-  //     .expect(200, {
-  //       error: 'Unauthorized'
-  //     }, done);
-  // });
-  // it('/api/users/all', (done) => {
-  //   request(server)
-  //     .get('/api/users/all')
-  //     .set('Accept', 'application/json')
-  //     .expect(200, {
-  //       error: 'Unauthorized'
-  //     }, done);
-  // });
 
 });
