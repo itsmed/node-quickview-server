59349974a9ccae60130fa59db6395b0aeb26e0cf
DejonGill <dejonjgill@gmail.com>
2017-04-21 23:22:10 -0700

    renamed

--- test/routes/transactions/test-transactions-new.js
+++
@@ -1,9 +0,0 @@
-const request = require('request');
-const { expect } = require('chai');
-
-describe('/api/transactions/new', () => {
-  it('saves a user', (done) => {
-    
-    done();
-  });
-});
