b3c248200cc8a7134e0af112cc74a8e63110863f
DejonGill <dejonjgill@gmail.com>
2017-04-25 10:46:38 -0700

    needs testing

--- server/routes/employees/create-employee.js
+++ server/routes/employees/create-employee.js
@@ -3,5 +3,23 @@ const mongoose = require('mongoose');
 const Employee = require('../../db/employee/employee-schema');
 
 module.exports = function createNewEmployee(req, res) {
-  const { username, permissions, name, picture } = req.body;
+  const { permissions, name, picture, isActive } = req.body.new_employee;
+  let employee = {
+    empId: randId(),
+    index: Math.floor(Math.random() * 30) + 20,
+    isActive,
+    name: {
+      first: name.first,
+      last: name.last
+    },
+    picture,
+    permissions,
+    email: name.first.concat(name.last, '@quickview.com'),
+    company: 'quickview'
+  }
 };
+
+
+function randId() {
+  return Math.random() * 120;
+}
